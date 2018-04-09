47431eb3bef7ec6144436c428dfe3399c86e4067
kathiravan22 <37337254+kathiravan22@users.noreply.github.com>
2018-04-09 18:07:59 +0530

    Create prime.py

---
+++ prime.py
@@ -0,0 +1,10 @@
+number = int(input("Enter any number"))
+if number > 1:
+    for i in range(2, number):
+        if (number % i) == 0:
+            print(number, "is not a prime number")
+            break
+    else:
+        print("it is a prime number")
+else:
+    print("it is not a prime number")
